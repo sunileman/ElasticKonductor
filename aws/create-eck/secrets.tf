a4ce8e0b0480d56111cc4de07b278c7fabda113d
root <root@ip-10-0-9-248.ec2.internal>
2023-01-18 16:09:47 +0000

    bug - dup loadbalancer

--- aws/create-eck/secrets.tf
+++ aws/create-eck/secrets.tf
@@ -1,4 +1,4 @@
-resource "kubectl_manifest" "loadbalancer" {
+resource "kubectl_manifest" "secrets" {
     for_each  = toset(data.kubectl_path_documents.aws-secrets.documents)
     yaml_body = each.value
 }
