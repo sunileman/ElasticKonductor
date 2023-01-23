15b6588b42b6f38fc1900b8cd34ce7cb34d1984c
root <root@ip-10-0-9-248.ec2.internal>
2023-01-23 18:39:20 +0000

    updated read.me and removed numeric from lbnames (azure)

--- azure/create-eck/namegen/lbname.tf
+++ azure/create-eck/namegen/lbname.tf
@@ -2,10 +2,12 @@ resource "random_string" "lbname" {
   length  = 16
   special = false
   upper   = false
+  numeric = false
 }
 
 resource "random_string" "lb2name" {
   length  = 16
   special = false
   upper   = false
+  numeric = false
 }
